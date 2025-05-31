ab66a0af53e1003d3fdf4d21f95ca895288df96a
@hexlet/cli <support@hexlet.io>
2025-05-31 11:20:10 +0100

    submit java-spring-boot-ru/http-crud

--- java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -3,7 +3,14 @@ package exercise;
 import exercise.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
