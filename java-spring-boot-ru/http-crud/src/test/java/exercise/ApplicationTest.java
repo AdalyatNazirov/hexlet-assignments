a1cb9c38ba38998b2698e836acdfe30fcb035956
@hexlet/cli <support@hexlet.io>
2025-05-31 11:22:16 +0100

    submit java-spring-boot-ru/http-crud

--- java-spring-boot-ru/http-crud/src/test/java/exercise/ApplicationTest.java
+++ java-spring-boot-ru/http-crud/src/test/java/exercise/ApplicationTest.java
@@ -1,23 +1,28 @@
 package exercise;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import exercise.model.Post;
-import exercise.util.TestUtil;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.nio.charset.StandardCharsets;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.http.MediaType;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import exercise.model.Post;
+import exercise.util.TestUtil;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
