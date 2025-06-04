ce2a4f15cadf3aca19629fc6fe8cd224a8260888
@hexlet/cli <support@hexlet.io>
2025-06-04 22:01:17 +0100

    submit java-spring-boot-ru/jpa-data

---
+++ java-spring-boot-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,47 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person show(@PathVariable long id) {
+        return personRepository.findById(id).get();
+    }
+
+    // BEGIN
+
+    @GetMapping(path = "")
+    public List<Person> list() {
+        return personRepository.findAll();
+    }
+
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person show(@RequestBody Person person) {
+        personRepository.save(person);
+        return person;
+    }
+
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable long id) {
+        personRepository.deleteById(id);
+    }
+
+
+    // END
+}
