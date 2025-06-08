33e43c4a6f31499efa09de102483114bebaed9e2
@hexlet/cli <support@hexlet.io>
2025-06-08 09:20:40 +0100

    submit java-spring-boot-ru/jpa-repository

---
+++ java-spring-boot-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,37 @@
+package exercise.controller;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // BEGIN
+    @GetMapping(path = "")
+    public List<Product> index(@RequestParam(required = false) Integer min, @RequestParam(required = false) Integer max) {
+        min = min == null ? 0 : min;
+        max = max == null ? Integer.MAX_VALUE : max;
+
+        return productRepository.findByPriceBetween(min, max, Sort.sort(Product.class).by(Product::getPrice));
+    }
+    // END
+
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+}
